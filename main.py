8675f02c552799ee512ade5a4731906b398fd3cd
Ishanpreet Singh <meishanpreetsingh5@gmail.com>
2024-05-24 20:11:38 -0400

    some changes

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from typing import List
-import uvicorn
 
 app = FastAPI()
 
 # CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins, for specific origins, use the list of allowed origins
+    allow_origins=["http://localhost:3000"],  # React app origin
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
-
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-class Aeroplanedetails(BaseModel):
-    id: int
-    num_seates: int
-    name_aero: str
-aeroplane_details = [
-    Aeroplanedetails(id=1, num_seates=15,name_aero= "Aircanada" ),
-    Aeroplanedetails(id=2, num_seates=20,name_aero= "Lufthansa" ),
-    Aeroplanedetails(id=3, num_seates=18,name_aero= "Airindia" ),
-]
-@app.get("/aeroplane", response_model=list[Aeroplanedetails])
-def return_aeroplane():
-    return aeroplane_details
+
+balance = 10000
+
+@app.get("/balance")
+def get_balance():
+    return {"balance": balance}
+
+@app.get("/deposit")
+def deposit(amount: int = Query(..., ge=0)):
+    global balance
+    balance += amount
+    return {"balance": balance}
+
+@app.get("/withdraw")
+def withdraw(amount: int = Query(..., ge=0)):
+    global balance
+    if amount > balance:
+        raise HTTPException(status_code=400, detail="Not Enough funds!")
+    balance -= amount
+    return {"balance": balance}
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
